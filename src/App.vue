edb0c48fcca112c6b5c9ca9e14c28a27713002dc
elrilacho <80546125+elrilacho@users.noreply.github.com>
2023-03-14 09:44:42 -0500

    Ajustes

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,27 @@
 <script setup>
-import HelloWorld from "./components/HelloWorld.vue";
-import navbar from "./components/navBar.vue";
-import cards from "./components/cards.vue";
+import banner from "./components/principal-banner.vue";
+import navbar from "./components/barra-navegacion.vue";
+import cards from "./components/cards-projects.vue";
 </script>
 
 <template lang="pug">
 navbar
-br
-HelloWorld
-cards 
+banner
+cards.mb-5
+.footer(style="background-color: black; width: 100% ").p-4
+  .row.justify-content-center
+    .col-lg-3
+      .text-white 
+        h3 Contact
+        p.mb-0 Addres
+          br 
+          | Number
+          br 
+          | City
+    .col-lg-3
+      .text-white 
+        p.mb-0.fst-italic Mollit commodo sit aliquip ex nostrud cillum incididunt exercitation in aliquip enim ipsum do amet. Id elit est Lorem reprehenderit commodo quis velit veniam commodo ad occaecat proident 
+
 </template>
 
 <style scoped>
